7a3bec62db23b67ebbca0f818fd96e1f0764ecb1
leithdm <leithdm@gmail.com>
2020-11-02 17:37:18 +0000

    feat: Add helper method to convert degrees to radians

--- assets/testing/spec/ship.test.js
+++ assets/testing/spec/ship.test.js
@@ -16,6 +16,11 @@ describe("ship rotation", () => {
     ship.rotate(RIGHT);
     expect(ship.angle).toBe(5);
   });
+  it("ship should rotate to the left by -5 degrees when ship moved to the left x1 time", () => {
+    expect(ship.rotate).toBeDefined();
+    ship.rotate(LEFT);
+    expect(ship.angle).toBe(-5);
+  });
   it("ship should rotate to the right by 360 degrees when ship is moved to the right x72 times", () => {
     expect(ship.rotate).toBeDefined();
     for (let i = 0; i < 72; i++) {
@@ -29,5 +34,21 @@ describe("ship rotation", () => {
       ship.rotate(LEFT);
     }
     expect(ship.angle).toBe(-360);
+  });
+});
+
+
+describe("convert ship angle to radians", () => {
+  it("should convert 0 degrees starting position to 0 radians", () => {
+    expect(convertAngleToRadians(ship.angle)).toBeDefined();
+    expect(convertAngleToRadians(ship.angle)).toBe(0);
+  });
+  it("should convert 90 degrees to approx. 1.571 radians", () => {
+      ship.angle = 90;
+      expect(convertAngleToRadians(ship.angle)).toBeCloseTo(1.57, 2);
+  });
+  it("should convert 180 degrees to approx. 3.141 radians", () => {
+      ship.angle = 180;
+      expect(convertAngleToRadians(ship.angle)).toBeCloseTo(3.14, 2);
   });
 });
--- assets/testing/src/game.js
+++ assets/testing/src/game.js
@@ -3,3 +3,8 @@ let canvasWidth = 800;
 let canvasHeight = 600; 
 const RIGHT = 1; 
 const LEFT = -1; 
+
+
+function convertAngleToRadians(angle) {
+    return angle * (Math.PI / 180);
+}
