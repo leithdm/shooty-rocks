0192af0e39bda87de679a0848c13f693faf88496
leithdm <leithdm@gmail.com>
2020-11-04 09:46:42 +0000

    feat: Create canvas element

--- assets/testing/src/game.js
+++ assets/testing/src/game.js
@@ -5,6 +5,19 @@ const RIGHT = 1;
 const LEFT = -1; 
 
 
+window.onload = () => {
+    setupCanvas(); 
+}
+
+function setupCanvas() {
+    canvas = document.getElementById("canvas"); 
+    context = canvas.getContext("2d");
+    context.fillStyle = "black";
+    canvas.width = canvasWidth; 
+    canvas.height = canvasHeight; 
+    context.fillRect(0, 0, canvas.width, 600); 
+}
+
 function convertAngleToRadians(angle) {
     return angle * (Math.PI / 180);
 }
