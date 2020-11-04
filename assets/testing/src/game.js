8d080c9feceecdd95490df5036fe867a988b890d
leithdm <leithdm@gmail.com>
2020-11-04 10:46:52 +0000

    feat: Place ship on the centre of canvas

--- assets/testing/src/game.js
+++ assets/testing/src/game.js
@@ -1,6 +1,7 @@
 let canvas; 
 let canvasWidth = 800; 
-let canvasHeight = 600; 
+let canvasHeight = 600;
+let ship;  
 const RIGHT = 1; 
 const LEFT = -1; 
 
@@ -16,6 +17,14 @@ function setupCanvas() {
     canvas.width = canvasWidth; 
     canvas.height = canvasHeight; 
     context.fillRect(0, 0, canvas.width, 600); 
+
+    //create a new ship
+    ship = new Ship();  
+    render(); 
+}
+
+function render() {
+    ship.drawShip(); 
 }
 
 function convertAngleToRadians(angle) {
